7e66143fb465966e3f739a1020c917fe37448358
Votre Nom <1261362@bdeb.qc.ca>
2024-10-13 13:31:12 -0400

    Ajout dune page html pour afficher les lien

--- TP1_A16/wwwroot/js/site.js
+++ TP1_A16/wwwroot/js/site.js
@@ -2,15 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function verifyPassword() {
+function verifierPassword() {
     const passwordInput = document.getElementById("Password");
+    const nameInput = document.getElementById("Name")
     const correctPassword = "gab2024";
-    if (passwordInput.value == correctPassword) {
-        document.getElementById("lienPage").style.display = "block"; // Afficher le conteneur des liens
+
+    if (passwordInput.value === correctPassword) {
+        window.location.href = '/html/modifierDonner.html'; // Redirection vers la page modifierDonner.html
+        return false; 
     } else {
         alert("Mot de passe incorrect !");
+        return false; 
     }
-
-    return false; 
 }
 
